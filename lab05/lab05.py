f9d0b0aeec52382ee70af29b3036c69a1ec5fdde
boodayAN <131428876+boodayAN@users.noreply.github.com>
2024-03-02 10:59:07 +0300

    Add files via upload

---
+++ lab05/lab05.py
@@ -0,0 +1,35 @@
+
+import turtle
+import time
+turtle.shape("turtle")
+turtle.speed(15)
+color = input("введите цвет")
+turtle.color(color)
+# дверь квадрат
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(90)
+# дом квадрат
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(135)
+turtle.left(90)
+turtle.forward(135)
+turtle.left(90)
+turtle.forward(135)
+turtle.left(90)
+turtle.forward(135)
+turtle.left(90)
+turtle.forward(135)
+# крыша треугольник
+turtle.left(45)
+turtle.forward(96)
+turtle.left(90)
+turtle.forward(96)
+
+time.sleep(10)
