914a621d6619132d1f9ef814726bf31b4f728bc1
boodayAN <131428876+boodayAN@users.noreply.github.com>
2024-03-02 09:54:59 +0300

    Add files via upload

---
+++ lab02/lab02_2.py
@@ -0,0 +1,7 @@
+chis1 = int(input("Введите 1 число:"))
+chis2 = int(input("Введите 2 число:"))
+chis3 = int(input("Введите 3 число:"))
+chis4= int(input("Введите 4 число:"))
+chis5 = int(input("Введите 5 число:"))
+sred = (chis1 + chis2 + chis3 + chis4 + chis5) / 5
+print("Среднее арифметическое", sred)
