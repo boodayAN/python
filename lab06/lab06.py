c222ec558dc8b72d8a175eceb09cf507641750a4
boodayAN <131428876+boodayAN@users.noreply.github.com>
2024-03-02 11:09:01 +0300

    Add files via upload

---
+++ lab06/lab06.py
@@ -0,0 +1,21 @@
+
+import turtle
+import time
+turtle.shape("turtle")
+turtle.speed(15)
+color = input("введите цвет")
+turtle.color(color)
+
+
+turtle.forward(350)
+turtle.left(144)
+turtle.forward(350)
+turtle.left(144)
+turtle.forward(350)
+turtle.left(144)
+turtle.forward(350)
+turtle.left(144)
+turtle.forward(350)
+turtle.left(144)
+
+time.sleep(10)
